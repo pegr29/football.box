5d127f7c604299065004149caf09a12c7dcdc28c
pegr29 <pegr29@mail.ru>
2019-02-23 16:05:36 +0300

    football.box

---
+++ js/script.js
@@ -0,0 +1,10 @@
+ // grecaptcha.ready(function() {
+ //      grecaptcha.execute('reCAPTCHA_site_key', {action: 'homepage'}).then(function(token) {
+ //         ...
+ //      });
+ //  });
+
+ $(document).ready(function(){
+      $('.slider').bxSlider();
+    });
+
